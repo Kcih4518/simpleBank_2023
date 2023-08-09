0a4c7a86c3adcdfbecfad809ab1ad3a670168c65
kcih4518 <kcih4518@gmail.com>
2023-08-09 20:52:08 +0800

    refactor: Extract createRandomAccount from TestCreateAccount

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
+func createRandomAccount(t *testing.T) Account {
 	arg := CreateAccountParams{
 		Owner:    util.RandomOwner(),
 		Balance:  util.RandomMoney(),
@@ -22,4 +22,10 @@ func TestCreateAccount(t *testing.T) {
 	require.Equal(t, arg.Currency, account.Currency)
 	require.NotZero(t, account.ID)
 	require.NotZero(t, account.CreatedAt)
+
+	return account
+}
+
+func TestCreateAccount(t *testing.T) {
+	createRandomAccount(t)
 }
