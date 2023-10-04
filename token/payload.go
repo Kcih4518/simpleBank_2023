31fce337a169d08f85582c7a363429efa1cd9b9c
kcih4518 <kcih4518@gmail.com>
2023-10-04 09:29:14 +0800

    feat: Add NewPayload() function for Payload creation

--- token/payload.go
+++ token/payload.go
@@ -15,3 +15,18 @@ type Payload struct {
 	IssuedAt  time.Time `json:"issued_at"`
 	ExpiredAt time.Time `json:"expired_at"`
 }
+
+// NewPayload will return payload or error.
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	tokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+	payload := &Payload{
+		ID:        tokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
